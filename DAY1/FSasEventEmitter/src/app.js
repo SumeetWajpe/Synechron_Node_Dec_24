d3c1ecac0b7a4fce0ede6b0e7c928c342f556ae7
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-12-02 13:45:42 +0530

    ✨ Basic Node Server

--- DAY1/FSasEventEmitter/src/app.js
+++ DAY1/FSasEventEmitter/src/app.js
@@ -2,17 +2,19 @@ const fs = require("fs");
 
 const readStream = fs.createReadStream("src/Input.txt", "utf-8"); // returns an EventEmitter instance
 const writeStream = fs.createWriteStream("src/Output.txt", "utf-8"); // returns an EventEmitter instance
-let dataToBeWritten = "";
+// let dataToBeWritten = "";
 
-readStream.on("data", chunk => {
-  console.log(">>>>>>>>>>>>>> Chunk received >>>>>>>>>>>>>>>> \n\r");
-  dataToBeWritten += chunk;
-});
+// readStream.on("data", chunk => {
+//   console.log(">>>>>>>>>>>>>> Chunk received >>>>>>>>>>>>>>>> \n\r");
+//   dataToBeWritten += chunk;
+// });
 
-readStream.on("end", () => {
-  console.log(
-    "Reading of the file done.. Read Stream ended - Now writing the file - Output.txt!",
-  );
+// readStream.on("end", () => {
+//   console.log(
+//     "Reading of the file done.. Read Stream ended - Now writing the file - Output.txt!",
+//   );
 
-  writeStream.write(dataToBeWritten);
-});
+//   writeStream.write(dataToBeWritten);
+// });
+
+readStream.pipe(writeStream);
