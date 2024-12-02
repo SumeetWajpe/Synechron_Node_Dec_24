5cae418bc0da4c9f663617070d0e25ee2d162774
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-12-02 13:25:34 +0530

    ✨ Using createWriteStream

--- DAY1/FSasEventEmitter/src/app.js
+++ DAY1/FSasEventEmitter/src/app.js
@@ -1,12 +1,18 @@
 const fs = require("fs");
 
 const readStream = fs.createReadStream("src/Input.txt", "utf-8"); // returns an EventEmitter instance
+const writeStream = fs.createWriteStream("src/Output.txt", "utf-8"); // returns an EventEmitter instance
+let dataToBeWritten = "";
 
 readStream.on("data", chunk => {
   console.log(">>>>>>>>>>>>>> Chunk received >>>>>>>>>>>>>>>> \n\r");
-//   console.log(chunk);
+  dataToBeWritten += chunk;
 });
 
 readStream.on("end", () => {
-  console.log("Reading of the file done.. Stream ended !");
+  console.log(
+    "Reading of the file done.. Read Stream ended - Now writing the file - Output.txt!",
+  );
+
+  writeStream.write(dataToBeWritten);
 });
