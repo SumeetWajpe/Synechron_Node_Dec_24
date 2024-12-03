f8ee89597a8ea776e166e971e467db6f5fc71719
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-12-03 11:38:46 +0530

    ✨ Using Websockets (server peer sending msgs to client peer)

--- DAY2/Websockets/src/app.js
+++ DAY2/Websockets/src/app.js
@@ -1,5 +1,7 @@
 const { createServer } = require("node:http"); // builtin
 const fs = require("fs");
+const socket = require("socket.io");
+
 const hostname = "127.0.0.1";
 const port = 3000;
 const readStream = fs.createReadStream("src/Index.html", "utf-8");
@@ -11,6 +13,14 @@ const server = createServer((req, res) => {
   //   });
 
   readStream.pipe(res);
+});
+
+var io = socket(server);
+io.sockets.on("connection", skt => {
+  setInterval(() => {
+    let dataToBeSent = new Date();
+    skt.emit("msg_from_server", dataToBeSent);
+  }, 2000);
 });
 
 server.listen(port, hostname, () => {
