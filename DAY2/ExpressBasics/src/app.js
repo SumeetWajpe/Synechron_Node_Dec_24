4d37ce7a4049e30c30d7deb925733f7df83d6e0d
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-12-03 13:08:50 +0530

    ✨ Sending styles.css with sendFile

--- DAY2/ExpressBasics/src/app.js
+++ DAY2/ExpressBasics/src/app.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.sendFile("Index.html", { root: __dirname });
+});
+app.get("/styles.css", (req, res) => {
+  res.sendFile("styles.css", { root: __dirname });
 });
 
 app.listen(port, () => {
