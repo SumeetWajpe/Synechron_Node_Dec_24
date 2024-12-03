535fe0d6f79167c5e972f2ec87136d8c92ee8c37
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-12-03 13:51:30 +0530

    ✨ Adding a new product

--- DAY2/ExpressBasics/src/app.js
+++ DAY2/ExpressBasics/src/app.js
@@ -2,13 +2,28 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+let products = [
+  { id: 1, name: "Laptop", price: 100000 },
+  { id: 2, name: "Mobile", price: 50000 },
+];
+
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.sendFile("Index.html", { root: __dirname });
 });
 app.get("/styles.css", (req, res) => {
   res.sendFile("styles.css", { root: __dirname });
 });
+app.get("/products", (req, res) => {
+  res.json(products);
+});
 
+app.post("/products", (req, res) => {
+  console.log(req.body);
+  products.push(req.body);
+  res.send("Posted successfully !");
+});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
