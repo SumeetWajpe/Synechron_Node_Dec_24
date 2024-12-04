160e0946039dc03f1a5035941c39cbc107e06b94
Sumeet Wajpe <sumeet.wajpe@hotmail.com>
2024-12-04 11:37:01 +0530

    ✨ Display courses in UI

---
+++ DAY3/onlinetraining_express/public/script.js
@@ -0,0 +1,31 @@
+// make ajax request - fetch api / axios  -> /courses -> console.log(course)
+
+async function FetchCourses() {
+  const res = await fetch("http://localhost:3000/courses");
+  let listofcourses = await res.json();
+
+  for (const course of listofcourses) {
+    CreateCourseItem(course);
+  }
+}
+
+function CreateCourseItem(course) {
+  let courseList = document.getElementById("listofcourses");
+  let newCol = document.createElement("div");
+  newCol.setAttribute("class", "col-md-3");
+
+  newCol.innerHTML = `<div class="card mt-2">
+  <img src=${course.imageUrl} class="card-img-top" height="200px" alt=${course.title}>
+  <div class="card-body">
+    <h5 class="card-title">${course.title}</h5>
+    <p class="card-text">₹. ${course.price}</p>
+    <p class="card-text"> ${course.rating}</p>
+    <p class="card-text"> ${course.trainer}</p>
+    <button class="btn btn-outline-primary">${course.likes}</button>
+  </div>
+</div>`;
+
+  courseList.appendChild(newCol);
+}
+
+window.addEventListener("DOMContentLoaded", FetchCourses);
